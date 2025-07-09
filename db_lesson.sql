4e2628a59e623e2f22c952bb0f2ef9d86c7546b1
Haruka Shimura <y29814021@gmail.com>
2025-07-09 21:48:43 +0900

    営業部の人だけを年齢の昇順で取得

--- db_lesson.sql
+++ db_lesson.sql
@@ -60,3 +60,6 @@ created_at作成日時の昇順(ORDER BY)でレコードを並び替え表示
 
 --Q7
 SELECT * FROM people WHERE (age BETWEEN 20 AND 29 AND gender = 2) OR (age BETWEEN 40 AND 49 AND gender = 1) ;
+
+--Q8
+SELECT * FROM people WHERE department_id = 1 ORDER BY age ASC;
